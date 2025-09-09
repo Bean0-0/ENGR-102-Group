217dda5d604d5278253c8208b6162022cef3b08b
radmerh-byte <radmerh@tamu.edu>
2025-09-09 20:30:22 +0000

    pounds to newtons conversion

--- Lab 3/unit_conversions.py
+++ Lab 3/unit_conversions.py
@@ -14,7 +14,11 @@
 #
 #
 from math import*
+
+
 quantity = float(input("Please enter the quantity to be converted: "))
+
+#pounds --> newtons
 pounds_to_newtons_converter = 4.45
 newtons = quantity * pounds_to_newtons_converter
 print (f"{quantity:.2f} pounds force is equivalent to {newtons:.2f} newtons")
