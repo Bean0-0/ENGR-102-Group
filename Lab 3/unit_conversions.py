00928bbd0c4461fd3482c2ce6565e97dd790573d
nicostrickler <nicostrickler@tamu.edu>
2025-09-10 11:34:03 -0500

    Update unit_conversions.py

--- Lab 3/unit_conversions.py
+++ Lab 3/unit_conversions.py
@@ -5,7 +5,7 @@
 # Names: Benjamin Nasse 
 # Cameron Seal
 # Charles Thomas
-# Nicholas 
+# Nicholas Strickler
 # Radwan Merhebi
 # Section: 564
 # Assignment: Lab 3
@@ -26,11 +26,26 @@ print (f"{quantity:.2f} pounds force is equivalent to {newtons:.2f} newtons")
 #meters --> feet
 meters_to_feet_converter = 3.28
 feet = quantity * meters_to_feet_converter
-print (f"{quantity:.2f} meters is equivalent to {feet} feet")
+print (f"{quantity:.2f} meters is equivalent to {feet:.2f} feet")
 
 #atmospheres --> kilopascals
 atmospheres_to_kilopascals_converter = 101.33
 kilopascals = quantity * atmospheres_to_kilopascals_converter
-print (f"{quantity:.2f} atmospheres is equivalent to {kilopascals} kilopascals")
+print (f"{quantity:.2f} atmospheres is equivalent to {kilopascals:.2f} kilopascals")
+
+#watts to BTU
+watts_to_BTU_converter=3.41
+BTU=quantity*watts_to_BTU_converter
+print(f"{quantity:.2f} watts is equivalent to {BTU:.2f} BTU per hour")
+
+#liters per second to gallons
+lps_to_gpm_converter=15.8503
+GPM=quantity*lps_to_gpm_converter
+print(f"{quantity:.2f} liters per second is equivalent to {GPM:.2f} US gallons per minute")
+
+#celsius to farenheit
+faren=(9/5)*quantity+32
+print(f"{quantity:.2f} degrees celsius is is equivalent to {faren:.2f} degrees fahrenheit")
+
 
 
