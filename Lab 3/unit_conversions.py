72cdfeb73d5693c55bffaf0e83811cc1cc71d7eb
radmerh-byte <radmerh@tamu.edu>
2025-09-11 15:54:38 -0500

    Update unit_conversions.py (FIXED)

--- Lab 3/unit_conversions.py
+++ Lab 3/unit_conversions.py
@@ -19,33 +19,32 @@ from math import*
 quantity = float(input("Please enter the quantity to be converted: "))
 
 #pounds --> newtons
-pounds_to_newtons_converter = 4.45
+pounds_to_newtons_converter = 4.44822
 newtons = quantity * pounds_to_newtons_converter
 print (f"{quantity:.2f} pounds force is equivalent to {newtons:.2f} newtons")
 
 #meters --> feet
-meters_to_feet_converter = 3.28
+meters_to_feet_converter = 3.28084
 feet = quantity * meters_to_feet_converter
 print (f"{quantity:.2f} meters is equivalent to {feet:.2f} feet")
 
 #atmospheres --> kilopascals
-atmospheres_to_kilopascals_converter = 101.33
+atmospheres_to_kilopascals_converter = 101.325
 kilopascals = quantity * atmospheres_to_kilopascals_converter
 print (f"{quantity:.2f} atmospheres is equivalent to {kilopascals:.2f} kilopascals")
 
 #watts to BTU
-watts_to_BTU_converter=3.41
+watts_to_BTU_converter= 3.412141633
 BTU=quantity*watts_to_BTU_converter
 print(f"{quantity:.2f} watts is equivalent to {BTU:.2f} BTU per hour")
 
 #liters per second to gallons
-lps_to_gpm_converter=15.8503
+lps_to_gpm_converter= 15.850323140625
 GPM=quantity*lps_to_gpm_converter
 print(f"{quantity:.2f} liters per second is equivalent to {GPM:.2f} US gallons per minute")
 
 #celsius to farenheit
 faren=(9/5)*quantity+32
-print(f"{quantity:.2f} degrees celsius is is equivalent to {faren:.2f} degrees fahrenheit")
-
+print(f"{quantity:.2f} degrees Celsius is equivalent to {faren:.2f} degrees Fahrenheit")
 
 
